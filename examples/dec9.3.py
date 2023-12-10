a07e371cbdf31cb4cb9da92ac7cbf099447e6a5a
Amy <amyannnwong@gmail.com>
2023-12-09 22:37:20 -0500

    december 9 full selenium example

--- examples/dec9.3.py
+++ examples/dec9.3.py
@@ -44,88 +44,87 @@ link_list = []
 # 7. Loop through list of keywords
 for keyword in keywords:
 
-    # 8. Select the search box by using its ID
-    search = driver.find_element(By.ID, 'twotabsearchtextbox')  # error being thrown from this line
+  # 8. Select the search box by using its ID
+	search = driver.find_element(By.ID, 'twotabsearchtextbox')
 
-    # 9. Use send keys to enter text into the form field using .sendkeys()
-    search.send_keys(keyword)
+  # 9. Use send keys to enter text into the form field using .sendkeys()
+	search.send_keys(keyword)
 
-    # 10. Select the search button by using its ID
-    search_button = driver.find_element(By.ID, 'nav-search-submit-button')
+  # 10. Select the search button by using its ID
+	search_button = driver.find_element(By.ID, 'nav-search-submit-button')
 
-    # 11. Click the search button by using .click()
-    search_button.click()
+  # 11. Click the search button by using .click()
+	search_button.click()
 
-    # 12. Set wait to 5 seconds for the page to load
-    driver.implicitly_wait(5)
+  # 12. Set wait to 5 seconds for the page to load
+	driver.implicitly_wait(5)
 
-    # 13. Collect a list of the all divs which contain the search results
-    scraped_items = wait(driver, 10).until(EC.presence_of_all_elements_located((By.XPATH, '//div[contains(@class, "s-result-item s-asin")]')))
-
-
-    #14. Iterate through the list of divs to find specifics of each item in the search result
-    for item in scraped_items:
-
-        # 15. Find ASIN number
-        asin = item.get_attribute("data-asin")
-
-        # 16. Find name
-        # an XPATH helps you identify a specific element on a page
-        product_name = item.find_element(By.XPATH, './/span[@class="a-size-medium a-color-base a-text-normal"]')
+  # 13. Collect a list of the all divs which contain the search results
+	scraped_items = wait(driver, 10).until(EC.presence_of_all_elements_located((By.XPATH, '//div[contains(@class, "s-result-item s-asin")]')))
 
 
-        # 17. Find price
-        whole_price_list = item.find_elements(By.XPATH, './/span[@class="a-price-whole"]')
-        fraction_price_list = item.find_elements(By.XPATH, './/span[@class="a-price-fraction"]')
+  # 14. Iterate through the list of divs to find specifics of each item in the search result
+	for item in scraped_items:
 
-        # 18a. If whole_price_list and fraction_price_list are NOT empty, join the dollar and cent values int one amount
-        if whole_price_list != [] and fraction_price_list != []:
-            product_price = '.'.join([whole_price_list[0].text, fraction_price_list[0].text])
-        # 18b. Otherwise, set the price as 0
-        else:
-            product_price = 0
+    # 15. Find ASIN number
+		asin = item.get_attribute("data-asin")
 
-        # 19. Find ratings box
-        ratings_box_list = item.find_elements(By.XPATH, './/div[@class="a-row a-size-small"]/span')
+    # 16. Find name
+    # an XPATH helps you identify a specific element on a page
+		product_name = item.find_element(By.XPATH, './/span[@class="a-size-medium a-color-base a-text-normal"]')
 
-        # 20. Find ratings and ratings_num
-        # 20a. If ratings_box_list is NOT empty, set the value of product_ratings and product_ratings_num
-        if ratings_box_list != []:
-            product_ratings = ratings_box_list[0].get_attribute('aria-label')
-            product_ratings_num = ratings_box_list[1].get_attribute('aria-label')
-        # 20b. Otherwise, set both values of product_ratings and product_ratings_num as 0
-        else:
-            product_ratings, product_ratings_num = 0, 0
+    # 17. Find price
+		whole_price_list = item.find_elements(By.XPATH, './/span[@class="a-price-whole"]')
+		fraction_price_list = item.find_elements(By.XPATH, './/span[@class="a-price-fraction"]')
 
-        # 21. Find links of items
-        link = item.find_element(By.XPATH, './/a[@class="a-link-normal s-underline-text s-underline-link-text s-link-style a-text-normal"]').get_attribute("href")
+    # 18a. If whole_price_list and fraction_price_list are NOT empty, join the dollar and cent values int one amount
+		if whole_price_list != [] and fraction_price_list != []:
+			product_price = '.'.join([whole_price_list[0].text, fraction_price_list[0].text])
+    # 18b. Otherwise, set the price as 0
+		else:
+			product_price = 0
 
-        # 22. Append scraped information to respective lists
-        asin_list.append(asin)
-        product_name_list.append(product_name.text)
-        product_price_list.append(product_price)
-        product_ratings_list.append(product_ratings)
-        product_ratings_num_list.append(product_ratings_num)
-        link_list.append(link)
+    # 19. Find ratings box
+		ratings_box_list = item.find_elements(By.XPATH, './/div[@class="a-row a-size-small"]/span')
 
-        print(product_name.text)
-        print(product_price)
-        print(product_ratings)
-        print(product_ratings_num)
-        print(link)
-        print('\n')
+    # 20. Find ratings and ratings_num
+    # 20a. If ratings_box_list is NOT empty, set the value of product_ratings and product_ratings_num
+		if ratings_box_list != []:
+			product_ratings = ratings_box_list[0].get_attribute('aria-label')
+			product_ratings_num = ratings_box_list[1].get_attribute('aria-label')
+    # 20b. Otherwise, set both values of product_ratings and product_ratings_num as 0
+		else:
+			product_ratings, product_ratings_num = 0, 0
 
-    # 23. Clear the search field by using .clear() so that the next keyword in keywords list can be entered
-    driver.find_element(By.ID, 'twotabsearchtextbox').clear()
+    # 21. Find links of items
+		link = item.find_element(By.XPATH, './/a[@class="a-link-normal s-underline-text s-underline-link-text s-link-style a-text-normal"]').get_attribute("href")
 
-    # 24. Wait 5 seconds to search again 
-    time.sleep(5)
+    # 22. Append scraped information to respective lists
+		asin_list.append(asin)
+		product_name_list.append(product_name.text)
+		product_price_list.append(product_price)
+		product_ratings_list.append(product_ratings)
+		product_ratings_num_list.append(product_ratings_num)
+		link_list.append(link)
+
+		print(product_name.text)
+		print(product_price)
+		print(product_ratings)
+		print(product_ratings_num)
+		print(link)
+		print('\n')
+
+  # 23. Clear the search field by using .clear() so that the next keyword in keywords list can be entered
+	driver.find_element(By.ID, 'twotabsearchtextbox').clear()
+
+  # 24. Wait 5 seconds to search again 
+	time.sleep(5)
     
-    # 25. Wait 5 seconds for the page to load
-    driver.implicitly_wait(5)
+  # 25. Wait 5 seconds for the page to load
+	driver.implicitly_wait(5)
 
-    # 26. Remove the comment on the following line to close the browser after script completion
-    # driver.quit()
+  # 26. Remove the comment on the following line to close the browser after script completion
+  # driver.quit()
 
 # 27. Create a dictionary to store the data
 df = pd.DataFrame({
